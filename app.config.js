8c780e9fd12cb625a73374e3fab2a49dfdec3fef
James <James.zjgao@gmail.com>
2026-02-08 23:31:12 -0600

    v2.1.5发布上架

--- app.config.js
+++ app.config.js
@@ -20,12 +20,12 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.vouchap.app",
-      buildNumber: "7",
+      buildNumber: "8",
       associatedDomains: ["applinks:vouchap.com"]
     },
     android: {
       package: "com.vouchap.app",
-      versionCode: 20,
+      versionCode: 21,
       // 使用系统照片选择器，不再声明 READ_MEDIA_IMAGES，符合 Google Play 照片权限政策
       permissions: [
         "CAMERA",
